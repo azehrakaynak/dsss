65e6abb4cf873d0f1d234cc5a69ef15ec08f1315
azehrakaynak <kaynak_319@hotmail.com>
2023-11-12 12:42:05 +0100

    commit

--- math_quiz.py
+++ math_quiz.py
@@ -1,44 +1,53 @@
 import random
 
 
-def function_A(min, max):
+def randomInteger(min, max):
     """
-    Random integer.
+     Generates Random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def randomSign():
+
+    """
+     Generates Random operation.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def problemGenerator(n1, n2, o):
+    """
+     Generates problem.
+    """
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        try:
+            n1 = randomInteger(1, 10); n2 = randomInteger(1, 5); o = randomSign()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            PROBLEM, ANSWER = problemGenerator(n1, n2, o)
+            print(f"\nQuestion: {PROBLEM}")
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+            if useranswer == ANSWER:
+                print("Correct! You earned a point.")
+                s += -(-1)
+            else:
+                print(f"Wrong answer. The correct answer is {ANSWER}.")
+        except Exception as k:
+            print('Error : ' + str(k))
 
     print(f"\nGame over! Your score is: {s}/{t_q}")
 
